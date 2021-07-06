4be7be3685d9eafc9be8030fd3a3c7b1c5d8aeb1
Nick Schultz <nsschultz@gmail.com>
2021-07-06 00:05:38 -0500

    moved to custom table

---
+++ src/components/table/custom-table.js
@@ -0,0 +1,125 @@
+import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+
+import { Edit } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+import TableHeaderCell from './table-header-cell';
+import { makeStyles } from '@material-ui/styles';
+
+const applyFilter = (column, field) => {
+  if (column.lookup) return column.filterValue.length === 0 || column.filterValue.some(v => convertToNumber(v) === field);
+  if (column.type === 'numeric') return field === convertToNumber(column.filterValue);
+  return field && field.toLowerCase().includes(column.filterValue.toLowerCase());
+};
+
+const applyFilters = (columns, rows) => {
+  var columnsWithFilter = columns.filter((column) => column.filterValue);
+  if (columnsWithFilter.length == 0) return rows;
+  return rows.filter(row => columnsWithFilter.length == columnsWithFilter.filter(column => applyFilter(column, row[column.field])).length);
+};
+
+const convertToNumber = (val) => { return parseInt(val, 10); };
+
+const compare = (a, b, orderBy) => b[orderBy] < a[orderBy] ? -1 : b[orderBy] > a[orderBy] ? 1 : 0;
+
+const getAlign = (column) => column.type === 'numeric' ? 'right' : 'left';
+
+const getComparator = (order, orderBy) => (a, b) => compare(a, b, orderBy) * (order === 'desc' ? 1 : -1);
+
+const stableSort = (array, comparator) => {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    return (order !== 0) ? order : a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+};
+
+const useStyles = makeStyles({ container: { display: 'flex', maxHeight: 750, overflowX: 'auto' } });
+
+const TableFilter = ({columns, values}) => {
+  const classes = useStyles();
+  const [limit, setLimit] = useState(25);
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState(null);
+  const [page, setPage] = useState(0);
+  const [rowCount, setRowCount] = useState(0);
+  const [rows, setRows] = useState([]);
+  
+  useEffect(() => { setRows(buildRows(columns, values)); }, [limit, order, orderBy, page]);
+
+  const buildRows = (columns, rows) => {
+    const filteredRows = applyFilters(columns, rows);
+    setRowCount(filteredRows.length);
+    return stableSort(filteredRows, getComparator(order, orderBy))
+      .slice(page*limit, (page+1)*limit)
+      .map((row) => {
+        return (
+          <TableRow hover key={row.id}>
+            <TableCell>
+              <IconButton size='small'><Edit fontSize='inherit'/></IconButton>
+            </TableCell>
+            {columns.map((column) => 
+              <TableCell key={column.field} align={getAlign(column)}>{getValue(column, row[column.field])}</TableCell>
+            )}
+          </TableRow>
+        );
+      });
+  };
+
+  const buildSortHandler = (property) => (event) => handleRequestSort(event, property);
+
+  const getValue = (column, value) => column.format && typeof value === 'number' ? column.format(value) : column.lookup ? column.lookup[value] : value;
+    
+  const handleRequestSort = (event, property) => {
+    setOrder(orderBy === property && order === 'asc' ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const onHandleFilterChange = (field, filterValue) => {
+    columns.filter((column) => column.field === field).forEach((column) => column.filterValue = filterValue);
+    setRows(buildRows(columns, values));
+  };
+   
+  return (
+    <Box>
+      <Paper>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader size='small'>
+            <TableHead>
+              <TableRow>
+                <TableCell align='left'/>
+                {columns.map((column) => 
+                  <TableHeaderCell 
+                    buildSortHandler={(key) => buildSortHandler(key)}
+                    column={column} 
+                    onHandleFilterChange={onHandleFilterChange}
+                    getAlign={(column) => getAlign(column)}
+                    key={column.field} 
+                    order={order}
+                    orderBy={orderBy}
+                  />)}
+              </TableRow>
+            </TableHead>
+            <TableBody>{rows}</TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <TablePagination 
+        component='div' 
+        count={rowCount} 
+        onPageChange={(event, newPage) => setPage(newPage)} 
+        onRowsPerPageChange={(event) => setLimit(event.target.value)} 
+        page={page} 
+        rowsPerPage={limit} 
+        rowsPerPageOptions={[25,50,100]}/>
+    </Box>
+  );
+}
+
+TableFilter.propTypes = { 
+  columns: PropTypes.array.isRequired,
+  values: PropTypes.array.isRequired
+};
+
+export default TableFilter;
--- src/pages/players.js
+++ src/pages/players.js
@@ -1,13 +1,11 @@
-import { Box, Container, IconButton, Paper, Snackbar, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import { Box, Container, Snackbar } from '@material-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
 
 import Alert from '@material-ui/lab/Alert';
-import { Edit } from '@material-ui/icons';
+import CustomTable from '../components/table/custom-table';
 import { Helmet } from 'react-helmet';
-import TableHeaderCell from '../components/table/table-header-cell';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
-import { makeStyles } from '@material-ui/styles';
 
 const columns = [
   { title: 'BHQ ID', field: 'bhqId', type: 'numeric' },
@@ -24,21 +22,7 @@ const columns = [
   { title: 'Drafted %', field: 'draftedPercentage', type: 'numeric', format: (value) => value.toFixed(2) }
 ];
 
-const applyFilter = (column, field) => {
-  if (column.lookup) return column.filterValue.length === 0 || column.filterValue.some(v => convertToNumber(v) === field);
-  if (column.type === 'numeric') return field === convertToNumber(column.filterValue);
-  return field && field.toLowerCase().includes(column.filterValue.toLowerCase());
-};
-
-const applyFilters = (columns, rows) => {
-  var columnsWithFilter = columns.filter((column) => column.filterValue);
-  if (columnsWithFilter.length == 0) return rows;
-  return rows.filter(row => columnsWithFilter.length == columnsWithFilter.filter(column => applyFilter(column, row[column.field])).length);
-};
-
 const convertToNumber = (val) => { return parseInt(val, 10); };
-
-const compare = (a, b, orderBy) => b[orderBy] < a[orderBy] ? -1 : b[orderBy] > a[orderBy] ? 1 : 0;
 
 const fixPlayer = player => {
   player.type = convertToNumber(player.type);
@@ -48,73 +32,25 @@ const fixPlayer = player => {
   return player;
 };
 
-const getAlign = (column) => column.type === 'numeric' ? 'right' : 'left';
-
-const getComparator = (order, orderBy) => (a, b) => compare(a, b, orderBy) * (order === 'desc' ? 1 : -1);
-
-const stableSort = (array, comparator) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    return (order !== 0) ? order : a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-};
-
-const useStyles = makeStyles({ container: { display: 'flex', maxHeight: 750, overflowX: 'auto' } });
-
 export default () => {
-  const classes = useStyles();
   const isMountedRef = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [limit, setLimit] = useState(25);
   const [message, setMessage] = useState('');
   const [open, setOpen] = useState(false);
-  const [order, setOrder] = useState('asc');
-  const [orderBy, setOrderBy] = useState(null);
-  const [page, setPage] = useState(0);
-  const [playerCount, setPlayerCount] = useState(0);
   const [players, setPlayers] = useState([]);
-  const [rows, setRows] = useState([]);
   const [severity, setSeverity] = useState('');
   
   useEffect(() => { 
     isMountedRef.current = true;
     getPlayers();
     () => { isMountedRef.current = false; }
-   }, []);
-
-   useEffect(() => { 
-     setRows(buildRows(columns, players));
-   }, [limit, order, orderBy, page]);
-
-  const buildRows = (columns, rows) => {
-    const filteredRows = applyFilters(columns, rows);
-    setPlayerCount(filteredRows.length);
-    return stableSort(filteredRows, getComparator(order, orderBy))
-      .slice(page*limit, (page+1)*limit)
-      .map((row) => {
-        return (
-          <TableRow hover key={row.id}>
-            <TableCell>
-              <IconButton size='small'><Edit fontSize='inherit'/></IconButton>
-            </TableCell>
-            {columns.map((column) => 
-              <TableCell key={column.field} align={getAlign(column)}>{getValue(column, row[column.field])}</TableCell>
-            )}
-          </TableRow>
-        );
-      });
-  };
-
-  const buildSortHandler = (property) => (event) => handleRequestSort(event, property);
+  }, []);
 
   const getPlayers = () => {
     axios
     .get('http://baseball-player-api.schultz.local/api/player')
     .then(response => { 
       if (isMountedRef.current) {
-        setRows(buildRows(columns, response.data.players));
         setPlayers(response.data.players); 
         setIsLoading(false); 
       }
@@ -126,18 +62,6 @@ export default () => {
       setIsLoading(false);
     });
   }
-
-  const getValue = (column, value) => column.format && typeof value === 'number' ? column.format(value) : column.lookup ? column.lookup[value] : value;
-    
-  const handleRequestSort = (event, property) => {
-    setOrder(orderBy === property && order === 'asc' ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const onHandleFilterChange = (field, filterValue) => {
-    columns.filter((column) => column.field === field).forEach((column) => column.filterValue = filterValue);
-    setRows(buildRows(columns, players));
-  };
 
   const onRowUpdate = (newData, oldData) => {
     const fixedPlayer = fixPlayer(newData);
@@ -173,39 +97,7 @@ export default () => {
         <Container maxWidth={false}>
           {isLoading 
             ? <Typography align='left' color='textPrimary' variant='h4'>Loading Players...</Typography>
-            :
-              <Box>
-                <Paper>
-                  <TableContainer className={classes.container}>
-                    <Table stickyHeader size='small'>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell align='left'/>
-                          {columns.map((column) => 
-                            <TableHeaderCell 
-                              buildSortHandler={(key) => buildSortHandler(key)}
-                              column={column} 
-                              onHandleFilterChange={onHandleFilterChange}
-                              getAlign={(column) => getAlign(column)}
-                              key={column.field} 
-                              order={order}
-                              orderBy={orderBy}
-                            />)}
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>{rows}</TableBody>
-                    </Table>
-                  </TableContainer>
-                </Paper>
-                <TablePagination 
-                  component='div' 
-                  count={playerCount} 
-                  onPageChange={(event, newPage) => setPage(newPage)} 
-                  onRowsPerPageChange={(event) => setLimit(event.target.value)} 
-                  page={page} 
-                  rowsPerPage={limit} 
-                  rowsPerPageOptions={[25,50,100]}/>
-              </Box>}
+            : <CustomTable columns={columns} values={players} />}
         </Container>
       </Box>
       <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={open} autoHideDuration={2000} onClose={() => setOpen(false)}>
