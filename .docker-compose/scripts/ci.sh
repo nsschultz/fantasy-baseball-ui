bf959bf6a9db40b7650d419beb10bf4d32f8c20d
Nick Schultz <nschultz@nvisia.com>
2024-05-15 17:44:37 +0000

    add copilot & clean up compose issues

--- .docker-compose/scripts/ci.sh
+++ .docker-compose/scripts/ci.sh
@@ -2,5 +2,5 @@
 set -e
 EXIT_CODE=0
 docker compose -f .docker-compose/docker-compose-ci.yaml -p fantasy-baseball-ui up --build --exit-code-from ui || EXIT_CODE=$?
-docker compose -f .docker-composev/docker-compose-ci.yaml -p fantasy-baseball-ui down
+docker compose -f .docker-compose/docker-compose-ci.yaml -p fantasy-baseball-ui down
 exit $EXIT_CODE
