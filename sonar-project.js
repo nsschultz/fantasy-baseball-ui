220f2d2033296b88debd43bcdab25de30496c066
Nick Schultz <nsschultz@gmail.com>
2024-08-05 04:51:02 +0000

    fix project key and update import

--- sonar-project.js
+++ sonar-project.js
@@ -1,11 +1,11 @@
-const scanner = require("sonarqube-scanner");
+const scanner = require("sonarqube-scanner").default;
 
 scanner(
   {
     serverUrl: "https://sonarcloud.io/",
     token: "<<SONAR_TOKEN>>",
     options: {
-      "sonar.projectKey": "fantasy-baseball-ui",
+      "sonar.projectKey": "nsschultz_fantasy-baseball-ui",
       "sonar.sources": "./src",
       "sonar.exclusions": "**/*.test.js, **/index.js, **/reportWebVitals.js, **/setupTests.js",
       "sonar.tests": "./src",
