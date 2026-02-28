3bc30000b662e7a093519611dc36d202a3442c07
Nick Schultz <nsschultz@gmail.com>
2026-02-28 07:00:36 +0000

    minor cleanup

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
+export const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        node: "current",
       },
-    ],
-    ["@babel/preset-react", { runtime: "automatic" }],
+    },
   ],
-};
+  ["@babel/preset-react", { runtime: "automatic" }],
+];
