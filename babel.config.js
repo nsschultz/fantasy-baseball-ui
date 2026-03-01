385a87518e552d6afffb14d1b3ee5c152255bf45
Nick Schultz <nsschultz@gmail.com>
2026-03-01 06:46:59 +0000

    chore: add TypeScript config and update build/test configs

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 export const presets = [
+  ["@babel/preset-typescript"],
   [
     "@babel/preset-env",
     {
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react({ include: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"] })],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
+  server: { host: "0.0.0.0", port: 3000 },
   publicDir: "public",
 });
