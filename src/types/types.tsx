26236ca0f54af879fbf0c0842e5cb47f5cee5b80
Nick Schultz <nsschultz@gmail.com>
2026-03-07 07:16:06 +0000

    delete the old types file

--- src/types/types.tsx
+++
@@ -1,6 +0,0 @@
-export interface NotificationType {
-  readonly notificationKey: number;
-  readonly message: string;
-  readonly timestamp: number;
-  readonly type: "success" | "error" | "info";
-}
