b05f51b5e4ee7d4d918b4560341ababf404aae27
Nick Schultz <nsschultz@gmail.com>
2026-03-10 04:54:16 +0000

    modify the simple pages

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,27 +1,23 @@
 import { Box, Container, Typography } from "@mui/material";
 
 import { Helmet } from "react-helmet";
-import React from "react";
 
-/**
- * Creates a new instance of the not found page.
- * @returns A new instance of NotFound.
- */
-const NotFound = () => (
-  <>
-    <Helmet>
-      <title>Not Found | Fantasy Baseball Analyzer</title>
-    </Helmet>
-    <Box sx={{ backgroundColor: "background.default", display: "flex", flexDirection: "column", height: "100%", justifyContent: "center" }}>
-      <Container maxWidth="md">
-        <Typography align="center" color="textPrimary" variant="h1">
-          404: The page you are looking for is not here
-        </Typography>
-        <Typography align="center" color="textPrimary" variant="subtitle2">
-          Either the route is broken or never existed...
-        </Typography>
-      </Container>
-    </Box>
-  </>
-);
-export default NotFound;
+export default function NotFound() {
+  return (
+    <>
+      <Helmet>
+        <title>Not Found | Fantasy Baseball Analyzer</title>
+      </Helmet>
+      <Box sx={{ backgroundColor: "background.default", display: "flex", flexDirection: "column", height: "100%", justifyContent: "center" }}>
+        <Container maxWidth="md">
+          <Typography align="center" color="textPrimary" variant="h1">
+            404: The page you are looking for is not here
+          </Typography>
+          <Typography align="center" color="textPrimary" variant="subtitle2">
+            Either the route is broken or never existed...
+          </Typography>
+        </Container>
+      </Box>
+    </>
+  );
+}
